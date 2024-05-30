b7f5950c8cb47417aeb1b9882a2d6b9844b4378a
kaialtair <166514084+altairkaitou@users.noreply.github.com>
2024-05-30 17:40:39 +0700

    first commit

---
+++ src/queue.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int empty(struct queue_t *q)
+{
+	if (q == NULL)
+	{
+		return 1;
+	}
+	return (q->size == 0);
+}
+
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
+	/* TODO: put a new process to queue [q] */
+	q->proc[q->size] = proc;
+	q->size++;
+}
+
+struct pcb_t *dequeue(struct queue_t *q)
+{
+	/* TODO: return a pcb whose priority is the highest
+	 * in the queue [q] and remember to remove it from q
+	 * */
+	if (empty(q))
+	{
+		return NULL;
+	}
+	struct pcb_t *temp = q->proc[0];
+	for (int i = 0; i < q->size - 1; i++)
+	{
+		q->proc[i] = q->proc[i + 1];
+	}
+	q->proc[q->size - 1] = NULL;
+	q->size--;
+	return temp;
+}
